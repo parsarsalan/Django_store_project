7ebf6ebedc5148cb2325c6b433a997fe2bbb2216
pars arsalan <parsarsalan@gmail.com>
2023-04-22 22:10:43 +0430

    modified : setting.py and docker-compose.yml for environmet variable (django secret key)

--- config/settings.py
+++ config/settings.py
@@ -9,19 +9,24 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
-from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 import django.core.mail.backends.console
 
+from pathlib import Path
+from environs import Env
+
+
+# environment variables
+env = Env()
+env.read_env()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+#hz&aqp_e*1gfdu)-km&u^g^^vf+3el2l8v&+lwqn5=vj4(iq'
+SECRET_KEY = 'DJANGO_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
