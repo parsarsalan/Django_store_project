73e39d8730779a5853c8175599b2aacb47fdac58
pars arsalan <parsarsalan@gmail.com>
2023-04-28 22:10:42 +0430

    modified : Admin.py -> added Commentinline with Tabuler , products admin : aded CommentInLine

--- products/admin.py
+++ products/admin.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 from .models import Products, ProductsComments
 
 
+class CommentInLine(admin.TabularInline):
+    model = ProductsComments
+    fields = ('product', 'author', 'stars', 'active',)
+    extra = 1   # -> show only 1 extra comment field
+
+
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
     fields = ('title', 'user', 'active', 'price', 'description',)
+    inlines = [
+        CommentInLine        # -> show comment in line in admin page
+    ]
 
 
 @admin.register(ProductsComments)
