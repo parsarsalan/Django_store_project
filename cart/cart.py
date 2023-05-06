44a5fac5e2aa5a86abd4bc936e79eb7f7104fbcd
parsarsalan <amirarsalangj@gmail.com>
2023-05-06 19:32:52 +0430

    add help descriptions in Cart class .

--- cart/cart.py
+++ cart/cart.py
@@ -11,6 +11,9 @@ class Cart:
         self.cart = cart
 
     def add_to_cart(self, product, quantity=1):
+        """
+        add a specific product with quantity of it in the cart.
+        """
         product_id = str(product.id)
 
         if product_id not in self.cart:
@@ -22,12 +25,18 @@ class Cart:
         self.save_to_cart()
 
     def remove_from_cart(self, product):
+        """
+        remove specific product from the cart.
+        """
         product_id = str(product.id)
         if product_id in self.cart:
             del self.cart[product_id]
             self.save_to_cart()
 
     def __iter__(self):
+        """
+        get and iteration every object of a product who added in cart from database.
+        """
         product_ids = self.cart.keys()
         products = Products.objects.filter(id__in=product_ids)
         cart = self.cart.copy()
@@ -37,12 +46,21 @@ class Cart:
             yield value
 
     def save_to_cart(self):
+        """
+        it is saving changes to in session.
+        """
         self.session.modified = True
 
     def clear_all(self):
+        """
+        clear all the products in the cart.
+        """
         del self.session['cart']
         self.save_to_cart()
 
     def __len__(self):
+        """
+        get a len of all products already added in cart.
+        """
         return len(self.cart.keys())
 
