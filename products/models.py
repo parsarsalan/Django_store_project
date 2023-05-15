19ae5c43b9c44d1c6c869afc2fe2405c6815f92b
parsarsalan <amirarsalangj@gmail.com>
2023-05-15 19:42:49 +0430

    modified settings.py , _base.html, and product_detail.html , Added : rosetta app

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
 
 class Products(models.Model):
@@ -11,6 +12,7 @@ class Products(models.Model):
     datetime_modified = models.DateTimeField(auto_now=True)
     price = models.PositiveIntegerField(default=0)
     active = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='products/product_image/', verbose_name=_('image'))
 
     def __str__(self):
         return self.title
@@ -37,11 +39,10 @@ class ProductsComments(models.Model):
     product = models.ForeignKey(Products, on_delete=models.CASCADE, related_name='comments')
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
-    stars = models.CharField(max_length=10, choices=STARS_CHOICES, blank=True, verbose_name="Score of Product")
+    stars = models.CharField(max_length=10, choices=STARS_CHOICES, blank=True, verbose_name=_("Score of Product"))
     active = models.BooleanField(default=True)
 
-
-# -> Manager
+    # -> Manager
     objects = models.Manager()
     active_comments = CustomCommentManager()
 
